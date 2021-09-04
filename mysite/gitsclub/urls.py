00846afee9f6d05d6e2590522d10eef819c1637e
Hari Krishnan U <harikrishnan6336@cet.ac.in>
2021-09-04 20:20:38 +0530

    templates added

--- mysite/gitsclub/urls.py
+++ mysite/gitsclub/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('groups/', views.groups, name='groups'),
+    path('group/<int:id>', views.group, name='group'),
+
 ]
--- mysite/gitsclub/views.py
+++ mysite/gitsclub/views.py
@@ -5,4 +5,10 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, World")
+    return render(request, 'index.html')
+
+def groups(request):
+    return HttpResponse("Here Groups")
+
+def group(request,id):
+    return HttpResponse("Each group Here  " + str(id))
