d00be338b816079ffd3cc6487a474a5a90abf986
nmpai2001 <nandakishormpai@gmail.com>
2021-09-05 01:45:02 +0530

    front end for form and new memeber landing page

--- mysite/gitsclub/views.py
+++ mysite/gitsclub/views.py
@@ -5,14 +5,21 @@ from .models import User
 
 # Create your views here.
 
+
 def index(request):
     return render(request, 'index.html')
 
+
+def newmember(request):
+    return render(request, 'newmember.html')
+
+
 def groups(request):
     users = User.objects.all()
-    return render(request, 'groups.html',{'users':users})
+    return render(request, 'groups.html', {'users': users})
 
-def group(request,id):
+
+def group(request, id):
     return HttpResponse("Each group Here  " + str(id))
 
 # def about(request):
