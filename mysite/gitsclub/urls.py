c4c06382aa450244b427ec26d5c786b11287e0de
Hari Krishnan U <harikrishnan6336@cet.ac.in>
2021-09-05 00:48:50 +0530

    .

--- mysite/gitsclub/urls.py
+++ mysite/gitsclub/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    # path('#about',views.about, name='about'),
     path('groups/', views.groups, name='groups'),
     path('group/<int:id>', views.group, name='group'),
-
 ]
