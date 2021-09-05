75f9a8db025d807b8787e18169ad1a8b67f090a0
nmpai2001 <nandakishormpai@gmail.com>
2021-09-05 10:52:23 +0530

    user creation success

--- mysite/gitsclub/urls.py
+++ mysite/gitsclub/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     # path('#about',views.about, name='about'),
-    path('new', views.newmember, name='newmember'),
+    path('new/', views.newmember, name='newmember'),
     path('groups/', views.groups, name='groups'),
     path('group/<int:id>', views.group, name='group'),
 ]
